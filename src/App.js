96904489c064ae8106547e58245a3b2a52e872a6
Pablo Miranda <62686272+cocomiranda@users.noreply.github.com>
2022-04-28 18:06:21 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Crypto from './components/Crypto';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>React</h1> */}
+      <Crypto />
+    </div>
+
+  );
+}
+
+export default App;
